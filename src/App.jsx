107ae8d69e83cc8124d3b3dc273d711e2bd55b4f
ShakQl <halimovsvhzod@gmail.com>
2025-12-26 17:46:10 +0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+import './App.css'
+import Modal from './components/modal'
+
+export default function App() {
+  return (
+    <>
+      <Modal></Modal>
+    </>
+  )
+}
---
+++ src/components/modal.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+
+export default function modal() {
+    return (
+        <>
+            <div className="modal">
+
+            </div>
+        </>
+    )
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
